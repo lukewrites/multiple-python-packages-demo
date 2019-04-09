e51c114f5b76abebc0d672cdab53d10ed7053b41
Luke Petschauer <lpetschauer@tableau.com>
2019-04-09 14:24:44 -0700

    all branches

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,12 @@ if [[ "$TRAVIS_TAG" =~ main ]]; then
     cd master
     python setup.py sdist
     ls dist/
-    # twine upload --repository_url DEV_PYPI dist/*
+    twine upload --repository_url DEV_PYPI dist/*
 elif [[ "$TRAVIS_TAG" =~ secondary ]]; then
     cd secondary
     python setup.py sdist
     ls dist/
-    # twine upload --repository_url DEV_PYPI dist/*
+    twine upload --repository_url DEV_PYPI dist/*
 else
     echo "no tag"
 fi
