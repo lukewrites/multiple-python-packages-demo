e4ed9ff511d0a374027fb67b87d4f959732fda2b
Luke Petschauer <lpetschauer@tableau.com>
2019-04-09 14:00:37 -0700

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+if [[ "$TRAVIS_TAG" =~ master ]]; then
+    cd master
+    python setup.py sdist
+    ls dist/
+    # twine upload --repository_url DEV_PYPI dist/*
+elif [[ "$TRAVIS_TAG" =~ secondary ]]; then
+    cd secondary
+    python setup.py sdist
+    ls dist/
+    # twine upload --repository_url DEV_PYPI dist/*
+else
+    echo "no tag"
+fi
