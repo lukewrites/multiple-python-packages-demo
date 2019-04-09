a62dce7b93938a67b9794c6771f778e3ef9e95bd
Luke Petschauer <lpetschauer@tableau.com>
2019-04-09 14:42:14 -0700

    hardcode repo

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,12 @@ if [[ "$TRAVIS_TAG" =~ main ]]; then
     cd master
     python setup.py sdist
     ls dist/
-    twine upload --repository-url DEV_PYPI dist/*
+    twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 elif [[ "$TRAVIS_TAG" =~ secondary ]]; then
     cd secondary
     python setup.py sdist
     ls dist/
-    twine upload --repository-url DEV_PYPI dist/*
+    twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 else
     echo "no tag"
 fi
