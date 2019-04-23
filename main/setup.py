26531734aa6af3e29750d30a680bcdd58dfbb3bf
Luke Petschauer <lpetschauer@tableau.com>
2019-04-22 23:33:17 -0700

    main 0.0.10

--- main/setup.py
+++ main/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="primary-lukewrites",
-    version="0.0.9",
+    version="0.0.10",
     author="Luke Petschauer",
     author_email="lukewrites@github.com",
     description="A primary example package that is silly.",
